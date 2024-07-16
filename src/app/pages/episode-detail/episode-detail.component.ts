6e850afa9dc09a5b7b283a3b0e24ce88f0218517
juan.escobar <juan.escobar@nter.es>
2024-07-16 15:25:22 +0200

    Implement episode details cards with podcast link, episode title, description, and HTML content rendering
    - Created a left-side card similar to the previous view, featuring episode image, podcast title, author, and a brief episode description. Implemented links on the podcast title and author to navigate back to the episode list.
    - Added a right-side card displaying the episode title, detailed description, and a basic HTML5 audio player for podcast playback.
    - Implemented handling for HTML content within episode descriptions to render it correctly without escaping.

--- src/app/pages/episode-detail/episode-detail.component.ts
+++ src/app/pages/episode-detail/episode-detail.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Result } from 'src/app/interfaces/podcasts-detail.interface';
+import { PodcastService } from 'src/app/services/podcast.service';
 
 @Component({
   selector: 'app-episode-detail',
   templateUrl: './episode-detail.component.html',
-  styleUrls: ['./episode-detail.component.css']
+  styleUrls: ['./episode-detail.component.css'],
 })
-export class EpisodeDetailComponent {
+export class EpisodeDetailComponent implements OnInit {
+  podcast: Result | undefined;
+  episode: Result | undefined;
+  loading: boolean = true;
 
+  constructor(
+    private route: ActivatedRoute,
+    private podcastService: PodcastService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEpisodeDetail();
+  }
+
+  getEpisodeDetail(): void {
+    const podcastId = this.route.snapshot.paramMap.get('podcastId');
+    const episodeId = this.route.snapshot.paramMap.get('episodeId');
+
+    if (podcastId && episodeId) {
+      this.podcastService.getPodcastEpisodes(podcastId).subscribe((data) => {
+        // Llamada al servicio para obtener los episodios del podcast
+        this.podcast = data.results[0]; // Asignación del detalle del podcast obtenido
+        this.episode = data.results.find(
+          (ep: { trackId: number }) => ep.trackId === parseInt(episodeId, 10)
+        ); // Búsqueda y asignación del detalle del episodio específico
+        this.loading = false; // Indicación de que la carga ha finalizado
+      });
+    }
+  }
 }
